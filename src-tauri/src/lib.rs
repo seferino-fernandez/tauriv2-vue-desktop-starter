8f23a342d256a80f8582345bcdde803cf3aee3a9
Seferino Fernandez <seferinofz@gmail.com>
2025-07-11 17:54:53 -0700

    feat: Added Tauri single instance plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,7 +6,8 @@ pub mod plugins;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let mut builder = tauri::Builder::default();
+    let mut builder =
+        tauri::Builder::default().plugin(tauri_plugin_single_instance::init(|_, _, _| {}));
 
     // CrabNebula DevTools prevents other logging plugins from working
     // https://docs.crabnebula.dev/devtools/troubleshoot/log-plugins/
